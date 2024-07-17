0727b5b314e92c60adb846cce0acd0f77f6a5784
sbstleal <sebastiaodeoleal@gmail.com>
2024-07-16 23:38:49 -0300

    Added add to cart tests

--- cypress/e2e/sauceDemo.spec.js
+++ cypress/e2e/sauceDemo.spec.js
@@ -28,4 +28,19 @@ describe('Login Tests', () => {
       cy.get('.inventory_item').should('have.length.greaterThan', 0);
     });
   });
+
+  describe('Add to Cart Tests', () => {
+    beforeEach(() => {
+      cy.visit('https://www.saucedemo.com');
+      cy.get('#user-name').type('standard_user');
+      cy.get('#password').type('secret_sauce');
+      cy.get('#login-button').click();
+    });
+  
+    it('Should add a product to the cart', () => {
+      cy.get('.inventory_item').first().find('button').click();
+      cy.get('.shopping_cart_badge').should('contain', '1');
+    });
+  });
+  
   
