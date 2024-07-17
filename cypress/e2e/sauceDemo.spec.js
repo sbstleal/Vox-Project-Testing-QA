626e5d1fb8c3669f9966b4608535a825b2cecbe1
sbstleal <sebastiaodeoleal@gmail.com>
2024-07-16 23:39:14 -0300

    Added remove from cart tests

--- cypress/e2e/sauceDemo.spec.js
+++ cypress/e2e/sauceDemo.spec.js
@@ -43,4 +43,20 @@ describe('Login Tests', () => {
     });
   });
   
+  describe('Remove from Cart Tests', () => {
+    beforeEach(() => {
+      cy.visit('https://www.saucedemo.com');
+      cy.get('#user-name').type('standard_user');
+      cy.get('#password').type('secret_sauce');
+      cy.get('#login-button').click();
+      cy.get('.inventory_item').first().find('button').click();
+    });
+  
+    it('Should remove a product from the cart', () => {
+      cy.get('.shopping_cart_link').click();
+      cy.get('.cart_item').first().find('button').click();
+      cy.get('.cart_item').should('not.exist');
+    });
+  });
+  
   
